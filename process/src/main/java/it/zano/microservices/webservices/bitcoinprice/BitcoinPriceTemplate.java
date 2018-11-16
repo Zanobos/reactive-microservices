c4bf221bda38b8657c2fc22c887aa88dce9be1cd
Zanotti Andrea <a.zanotti@reply.it>
2018-11-16 12:03:14 +0100

    added custom converter

--- process/src/main/java/it/zano/microservices/webservices/bitcoinprice/BitcoinPriceTemplate.java
+++ process/src/main/java/it/zano/microservices/webservices/bitcoinprice/BitcoinPriceTemplate.java
@@ -2,6 +2,9 @@ package it.zano.microservices.webservices.bitcoinprice;
 
 import it.zano.microservices.webservices.rest.ArchRestTemplate;
 import it.zano.microservices.webservices.rest.ArchRestTemplateProperties;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,5 +16,15 @@ public class BitcoinPriceTemplate extends ArchRestTemplate<BitcoinPriceResource>
 
     public BitcoinPriceTemplate(ArchRestTemplateProperties properties) {
         super(properties, BitcoinPriceResource.class);
+        getMessageConverters().add(new JavaScriptMessageConverter());
+    }
+
+    //I create a new converter, that it's basically the same jackson, but it works with when header is javascript
+    private static class JavaScriptMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+        //Add a new mediatype converter
+        private JavaScriptMessageConverter() {
+            super(Jackson2ObjectMapperBuilder.json().build(),new MediaType("application","javascript"));
+        }
     }
 }
