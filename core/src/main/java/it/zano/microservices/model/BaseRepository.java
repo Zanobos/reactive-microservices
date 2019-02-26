1ea74eb771e9bec1a2fe06b96139bde26d98274f
Zanotti Andrea <a.zanotti@reply.it>
2019-02-26 11:07:50 +0100

    actuators

--- core/src/main/java/it/zano/microservices/model/BaseRepository.java
+++ core/src/main/java/it/zano/microservices/model/BaseRepository.java
@@ -1,10 +1,12 @@
 package it.zano.microservices.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * @author a.zanotti
  * @since 17/10/2018
  */
+@NoRepositoryBean
 public interface BaseRepository<E extends BaseEntity> extends JpaRepository<E, Integer> {
 }
